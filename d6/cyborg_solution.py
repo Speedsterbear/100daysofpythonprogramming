c9a6a72d30965643607a3adffa3d094adb7a3533
speedsterbear <e.gaona@outlook.com>
2023-08-07 11:36:47 -0600

    day6_done :)

---
+++ d6/cyborg_solution.py
@@ -0,0 +1,16 @@
+# def square():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# while not at_goal():
+#     if wall_in_front():
+#         turn_left()
+#         while front_is_clear():
+#             move()
+#             square()
+#     else:
+#         move()
+#
+#
